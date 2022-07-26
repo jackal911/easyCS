d1b5e0a1a1ca3234692c063190e4bec5ba5b5589
balor00 <thisisdiss@gmail.com>
2022-07-27 08:56:25 +0900

    깃 이그노어 해제

--- Programmers/MakePrime/MakePrime/Program.cs
+++ Programmers/MakePrime/MakePrime/Program.cs
@@ -57,6 +57,57 @@ namespace MakePrime
     }
     class Program
     {
+<<<<<<< HEAD
+        static IEnumerable<IEnumerable<T>> GetKCombs<T>(IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            IEnumerable<IEnumerable<T>> k = GetKCombs(list, length - 1)
+                .SelectMany(t => list.Where(o => o.CompareTo(t.Last()) > 0),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+            return k;
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+            foreach (var v in GetKCombs<int>(nums, 3))
+            {
+                v.ToList().ForEach(x => Console.Write(x + " "));
+                Console.WriteLine();
+            }
+            //
+            PetOwner[] petOwners =
+            { new PetOwner { Name="Higa",
+                  Pets = new List<string>{ "Scruffy", "Sam" } },
+              new PetOwner { Name="Ashkenazi",
+                  Pets = new List<string>{ "Walker", "Sugar" } },
+              new PetOwner { Name="Price",
+                  Pets = new List<string>{ "Scratches", "Diesel" } },
+              new PetOwner { Name="Hines",
+                  Pets = new List<string>{ "Dusty" } } };
+
+            // Project the pet owner's name and the pet's name.
+            var query =
+                petOwners
+                .SelectMany(petOwner => petOwner.Pets, (petOwner, petName) => new { petOwner, petName }) // 이중구조로 되어있는 PetOwner의 Pets 리스트를 풀어서 petOwner와 함께 anonymous type으로 묶는다.
+                .Where(ownerAndPet => ownerAndPet.petName.StartsWith("S")) // pet 이름이 S로 시작하는 멤버만 살린다.
+                .Select(ownerAndPet =>
+                        new List<string>()
+                        {
+                            ownerAndPet.petOwner.Name,
+                            ownerAndPet.petName
+                        }
+                ); // anonymous type으로 되어있는 각각의 멤버를 풀고 petOwner 객체에서 owner의 name만 빼서 다시 anonymous type으로 묶는다.
+
+            // Print the results.
+            foreach (var obj in query)
+            {
+                Console.WriteLine(obj); // anonymous type은 조회도 깔끔하게 잘된다.
+            }
+            Dictionary<string, string> dict = new Dictionary<string, string>() { { "a", "b" } };
+            Console.WriteLine(dict["a"]);
+            Console.WriteLine();
+=======
         
         static void Main(string[] args)
         {
@@ -69,6 +120,7 @@ namespace MakePrime
 //                 Console.WriteLine();
 //             }
 
+>>>>>>> 6ad0150df01da262fc98673dcc81565c47614e43
         }
     }
 }
