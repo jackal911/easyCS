153a96c4c79356dba7435de08b97e9195f220277
balor00 <thisisdiss@gmail.com>
2022-08-22 18:09:51 +0900

    백준 Step15 백트래킹 스도쿠 풀이 중

--- BackJun/Step15/Step15/Program.cs
+++ BackJun/Step15/Step15/Program.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Diagnostics;
+using System.Dynamic;
 
 namespace Step15
 {
     // Step15 - 백트래킹
     class Program
     {
+		// Q2580 - 스도쿠
+		static Dictionary<string, dynamic> solution = new Dictionary<string, dynamic>();
+		static IEnumerable<int> oneToNine = Enumerable.Range(1, 9);
+
         // Q15649 - N과 M (1)
         static void printPermutation(int M, List<int> lst, List<bool> visit, List<int> result, StreamWriter sw)
         {            
@@ -135,53 +139,68 @@ namespace Step15
             return completeCount;
         }
 
+		// Q2580 - 스도쿠
+// 		static string doSudoku(List<string> sudoku)
+// 		{
+// 			bool isFinished = false;
+// 			while (isFinished == false)
+// 			{
+// 				allCheck(sudoku);
+// 				if (solution.Count == 0)
+// 				{
+// 					isFinished = true;
+// 				}
+// 			}
+// 			
+// 		}
+
         static void Main(string[] args)
         {
-            /*
-            // Q15649 - N과 M (1)
-            int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            List<int> nums = Enumerable.Range(1, NM[0]).ToList();
-            List<bool> visit = Enumerable.Repeat(false, NM[0]).ToList();
-            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-            printPermutation(NM[1], nums, visit, new List<int>(), sw);
-            sw.Close();
+			/*
+			// Q15649 - N과 M (1)
+			int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			List<int> nums = Enumerable.Range(1, NM[0]).ToList();
+			List<bool> visit = Enumerable.Repeat(false, NM[0]).ToList();
+			StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+			printPermutation(NM[1], nums, visit, new List<int>(), sw);
+			sw.Close();
             
-            // Q15650 - N과 M (2)
-            int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            List<int> nums = Enumerable.Range(1, NM[0]).ToList();
-            int count = 0;
-            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-            printCombination(NM[0], NM[1], nums, new List<int>(), count, sw);
-            sw.Close();
+			// Q15650 - N과 M (2)
+			int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			List<int> nums = Enumerable.Range(1, NM[0]).ToList();
+			int count = 0;
+			StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+			printCombination(NM[0], NM[1], nums, new List<int>(), count, sw);
+			sw.Close();
             
-            // Q15651 - N과 M (3)
-            int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            List<int> nums = Enumerable.Range(1, NM[0]).ToList();
-            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-            printRepeatedPermutation(NM[1], nums, new List<int>(), sw);
-            sw.Close();
+			// Q15651 - N과 M (3)
+			int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			List<int> nums = Enumerable.Range(1, NM[0]).ToList();
+			StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+			printRepeatedPermutation(NM[1], nums, new List<int>(), sw);
+			sw.Close();
             
-            // Q15652 - N과 M (4)
-            int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            List<int> nums = Enumerable.Range(1, NM[0]).ToList();
-            int count = 0;
-            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-            printRepeatedCombination(NM[0], NM[1], nums, new List<int>(), count, sw);
-            sw.Close();
-            */
-            // Q9663 - N-Queen
-            int N = int.Parse(Console.ReadLine());
-            Stopwatch stopwatch = new Stopwatch(); //객체 선언
-            stopwatch.Start(); // 시간측정 시작
+			// Q15652 - N과 M (4)
+			int[] NM = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			List<int> nums = Enumerable.Range(1, NM[0]).ToList();
+			int count = 0;
+			StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+			printRepeatedCombination(NM[0], NM[1], nums, new List<int>(), count, sw);
+			sw.Close();
+            
+			// Q9663 - N-Queen
+			int N = int.Parse(Console.ReadLine());
+			Stopwatch stopwatch = new Stopwatch(); //객체 선언
+			stopwatch.Start(); // 시간측정 시작
 			
 			int[] coordinates = new int[15];
 			int completeCount = 0;
 			Console.WriteLine(NQueen(coordinates, N, ref completeCount));
 
-            stopwatch.Stop(); //시간측정 끝
+			stopwatch.Stop(); //시간측정 끝
 
-            System.Console.WriteLine("time : " +
-                               stopwatch.ElapsedMilliseconds + "ms");
+			System.Console.WriteLine("time : " +
+							   stopwatch.ElapsedMilliseconds + "ms");
             
 //             foreach (var row in chessBoard)
 //             {
@@ -191,9 +210,17 @@ namespace Step15
 //                 }
 //                 Console.WriteLine();
 //             }
-
-            // Q2580 - 스도쿠
-
+			*/
+			// Q2580 - 스도쿠
+			List<string> sudoku = new List<string>();
+			int[] a = new int[9];
+			for (int i = 0; i < 9; i++)
+			{
+				sudoku.Add(Console.ReadLine());
+			}
+			string b = "abc";
+			
+			Console.WriteLine("Hello");
             
             // Q14888 - 연산자 끼워넣기
 
