6fea1ef8c8cf34ca60a5c33e64b8149cf9178ab1
balor00 <thisisdiss@gmail.com>
2022-09-13 18:08:19 +0900

    프로그래머스 간단한 문제 하나 풀이

---
+++ Programmers/findWhoseFactorIsOne/findWhoseFactorIsOne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace findWhoseFactorIsOne
+{
+	class Program
+	{
+		public class Solution
+		{
+			public int solution(int n)
+			{
+				for (int i = 2; i <= Math.Sqrt(n); i++)
+				{
+					if ((n - 1) % i == 0)
+					{
+						return i;
+					}
+				}
+				return n - 1;
+			}
+		}
+		static void Main(string[] args)
+		{
+			Solution s = new Solution();
+			Console.WriteLine(s.solution(999998));
+		}
+	}
+}
