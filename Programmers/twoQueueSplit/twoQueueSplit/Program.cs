d47d6d719c6a9283269b887ed9f669344cfbd08d
balor00 <thisisdiss@gmail.com>
2022-09-13 10:15:52 +0900

    두 큐 풀이 완료

--- Programmers/twoQueueSplit/twoQueueSplit/Program.cs
+++ Programmers/twoQueueSplit/twoQueueSplit/Program.cs
@@ -17,22 +17,29 @@ namespace twoQueueSplit
 			{
 				q1 = new Queue<long>(Array.ConvertAll(queue1, s => (long)s));
 				q2 = new Queue<long>(Array.ConvertAll(queue2, s => (long)s));
-				maxCount = q1.Count + q2.Count;
+				maxCount = q1.Count + q2.Count + 1; // 넉넉하게 전체 Count * 2가 제일 안전함. +2는 현재의 모든 테스트 케이스 만족하는 최소 maxCount
 				return doShuffle(q1, q2);
 			}
 			private int doShuffle(Queue<long> q1, Queue<long> q2)
 			{
 				long q1Sum = q1.Sum();
-				long q2Sum = q1.Sum();
-				while (q1.Sum() != q2.Sum())
+				long q2Sum = q2.Sum();
+				while (q1Sum != q2Sum)
 				{
-					if (q1.Sum() > q2.Sum())
+					long minus;
+					if (q1Sum > q2Sum)
 					{
-						q2.Enqueue(q1.Dequeue());
+						minus = q1.Dequeue();
+						q2.Enqueue(minus);
+						q1Sum -= minus;
+						q2Sum += minus;
 					}
 					else
 					{
-						q1.Enqueue(q2.Dequeue());
+						minus = q2.Dequeue();
+						q1.Enqueue(minus);
+						q2Sum -= minus;
+						q1Sum += minus;
 					}
 					shuffleCount++;
 					if (shuffleCount > maxCount)
@@ -46,8 +53,8 @@ namespace twoQueueSplit
 		static void Main(string[] args)
 		{
 			Solution s = new Solution();
-			int[] queue1 = new int[] { 1, 1 };
-			int[] queue2 = new int[] { 1, 5 };
+			int[] queue1 = new int[] { 8, 1, 7, 45, 2 };
+			int[] queue2 = new int[] { 3, 4, 5, 6, 9 };
 			Console.WriteLine(s.solution(queue1, queue2));
 		}
 	}
