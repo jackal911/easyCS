516f3e2790fdc552be2d1a26e84315e5e15e0cac
balor00 <thisisdiss@gmail.com>
2022-09-14 18:01:37 +0900

    프로그래머스 여러문제 풀이

---
+++ Programmers/RepeatBinary/RepeatBinary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepeatBinary
+{
+	// 이진 변환 반복하기 - https://school.programmers.co.kr/learn/courses/30/lessons/70129
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
--- Programmers/findWhoseFactorIsOne/findWhoseFactorIsOne/Program.cs
+++ Programmers/findWhoseFactorIsOne/findWhoseFactorIsOne/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace findWhoseFactorIsOne
 {
+	// 나머지가 1이 되는 수 찾기 - https://school.programmers.co.kr/learn/courses/30/lessons/87389
 	class Program
 	{
 		public class Solution
