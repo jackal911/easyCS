e43ab99713f9fc3cb996e6b69763e667b485f874
balor00 <thisisdiss@gmail.com>
2022-09-15 18:00:08 +0900

    프로그래머스 문제 양껏 푼날

---
+++ Programmers/GcdAndLcm/GcdAndLcm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GcdAndLcm
+{
+	// 최대공약수와 최소공배수 - https://school.programmers.co.kr/learn/courses/30/lessons/12940
+	class Program
+	{
+		public class Solution
+		{
+			private static Func<int, int, int> calGcd = (num1, num2) => num1 % num2 == 0 ? num2 : calGcd(num2, num1 % num2);
+			public int[] solution(int n, int m)
+			{
+				int gcd = calGcd(n, m);
+				return new int[] { gcd, n * m / gcd };
+			}
+		}
+		static void Main(string[] args)
+		{
+			Solution s = new Solution();
+			int[] result = s.solution(2, 5);
+			Console.WriteLine("[{0},{1}]", result[0], result[1]);
+		}
+	}
+}
--- Programmers/RepeatBinary/RepeatBinary/Program.cs
+++ Programmers/RepeatBinary/RepeatBinary/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace RepeatBinary
 {
 	// 이진 변환 반복하기 - https://school.programmers.co.kr/learn/courses/30/lessons/70129
 	class Program
 	{
+		public class Solution
+		{
+			public int[] solution(string s)
+			{
+				int zeroCount = 0;
+				int convertCount = 0;
+				while (s != "1")
+				{
+					zeroCount += s.Count(x => x == '0');
+					int countOne = s.Count(x => x == '1');
+					s = Convert.ToString(countOne, 2);
+					convertCount++;
+				}
+				return new int[] { convertCount, zeroCount };
+			}
+		}
 		static void Main(string[] args)
 		{
+			Solution sol = new Solution();
+			string s = "110010101001";
+			int[] result = sol.solution(s);			
+			Console.WriteLine("[{0},{1}]", result[0], result[1]);
 		}
 	}
 }
