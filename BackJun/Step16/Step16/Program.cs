4a2ef2bdbad0edb567ee3169203c080b1ba492fc
jhlee <junho.lee@cadwin.co.kr>
2022-09-20 17:55:13 +0900

    백준 동적 계획법 1 풀이 중. 전깃줄 개빡셈

--- BackJun/Step16/Step16/Program.cs
+++ BackJun/Step16/Step16/Program.cs
@@ -105,38 +105,6 @@ namespace Step16
 
 		}
 
-		// Q1149 - RGB거리
-		class RGBStreet
-		{
-			//public int minSum;
-			public HashSet<int> sums = new HashSet<int>();
-			public int maxIdx;
-			public List<List<int>> RGBstreet;			
-			public RGBStreet(List<List<int>> inpStreet)
-			{
-				this.RGBstreet = inpStreet;
-				this.maxIdx = inpStreet.Count;
-				//this.minSum = int.MaxValue;
-			}
-			public void coloring(int curIdx, int removeColor = -1, int curSum = 0)
-			{
-				if (curIdx == maxIdx)
-				{
-					//Console.WriteLine(curSum);
-					//minSum = curSum > minSum ? minSum : curSum;
-					sums.Add(curSum);
-					return;
-				}
-				for (int i = 0; i < 3; i++)
-				{
-					if (i != removeColor)
-					{
-						coloring(curIdx + 1, i, curSum + RGBstreet[curIdx][i]);
-					}
-				}
-			}
-		}
-
 		static void Main(string[] args)
 		{
 			/*
@@ -207,7 +175,7 @@ namespace Step16
 				}
 			}
 			Console.WriteLine(max);
-			*/
+			
 			// Q1149 - RGB거리
 			int N = int.Parse(Console.ReadLine());
 			List<List<int>> inpStreet = new List<List<int>>();
@@ -215,33 +183,215 @@ namespace Step16
 			{
 				inpStreet.Add(Array.ConvertAll(Console.ReadLine().Split(), int.Parse).ToList());
 			}
-			RGBStreet rs = new RGBStreet(inpStreet);
-			rs.coloring(0);
-			Console.WriteLine(rs.sums.Min());
-
+			for (int i = 1; i < N; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					int house1 = (j + 2) % 3;
+					int house2 = (j + 1) % 3;
+					inpStreet[i][j] += Math.Min(inpStreet[i - 1][house1], inpStreet[i - 1][house2]);
+				}
+			}
+			Console.WriteLine(inpStreet[N - 1].Min());
+			
 			// Q1932 - 정수 삼각형
-
-
+			int n = int.Parse(Console.ReadLine());
+			int[][] triNums = new int[n][];
+			for (int i = 0; i < n; i++)
+			{
+				triNums[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			}
+			for (int i = 1; i < n; i++)
+			{
+				int length = triNums[i].Length;
+				// 첫점과 끝점 사전 작업
+				triNums[i][0] += triNums[i - 1][0];
+				triNums[i][length - 1] += triNums[i - 1][length - 2];
+				for (int j = 1; j < length - 1; j++)
+				{
+					triNums[i][j] += Math.Max(triNums[i - 1][j], triNums[i - 1][j - 1]);
+				}
+			}
+			Console.WriteLine(triNums[n - 1].Max());
+			
 			// Q2579 - 계단 오르기
-
+			int N = int.Parse(Console.ReadLine());
+			int[][] floor = new int[N][];
+			for (int i = 0; i < N; i++)
+			{
+				floor[i] = new int[2] { int.Parse(Console.ReadLine()), 0 };
+			}
+			if (N > 1)
+			{
+				floor[1][1] = floor[0][0] + floor[1][0];
+				for (int i = 2; i < N; i++)
+				{
+					floor[i][1] = floor[i][0] + floor[i - 1][0];
+					floor[i][0] += Math.Max(floor[i - 2][0], floor[i - 2][1]);
+				}
+			}
+			Console.WriteLine(floor[N-1].Max());
 			
 			// Q1463 - 1로 만들기
-
-
+			int N = int.Parse(Console.ReadLine());
+			int[] counts = new int[N + 1];
+			counts[1] = 0;
+			for (int i = 2; i <= N; i++)
+			{
+				int minCount = counts[i - 1];
+				if (i % 3 == 0 && counts[i / 3] < minCount)
+				{
+					minCount = counts[i / 3];
+				}
+				if (i % 2 == 0 && counts[i / 2] < minCount)
+				{
+					minCount = counts[i / 2];
+				}
+				counts[i] = minCount + 1;
+			}
+			Console.WriteLine(counts[N]);
+			
 			// Q10844 - 쉬운 계단 수
-
-
+			int N = int.Parse(Console.ReadLine());
+			int[] numCount = new int[10] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+			int i = 1;
+			while (i < N)
+			{
+				int[] tempCount = new int[10];
+				tempCount[0] = numCount[1];
+				tempCount[9] = numCount[8];
+				for (int j = 1; j < 9; j++)
+				{
+					tempCount[j] = (numCount[j - 1] + numCount[j + 1])%1000000000;
+				}
+				numCount = tempCount.ToArray();
+				i++;
+			}
+			int sum = 0;
+			foreach (int count in numCount)
+			{
+				sum += count;
+				sum %= 1000000000;
+			}
+			Console.WriteLine(sum);
+			
 			// Q2156 - 포도주 시식
-
-
+			int n = int.Parse(Console.ReadLine());
+			int[] grapeAlcohol = new int[n+1];
+			for (int i = 1; i <= n; i++)
+			{
+				grapeAlcohol[i] = int.Parse(Console.ReadLine());
+			}
+			int[] dp = new int[n + 1];
+			dp[1] = grapeAlcohol[1];
+			if (n > 1)
+			{
+				dp[2] = grapeAlcohol[1] + grapeAlcohol[2];
+			}
+			for (int i = 3; i <= n; i++)
+			{
+				dp[i] = Math.Max(Math.Max(dp[i - 1], dp[i - 3] + grapeAlcohol[i - 1] + grapeAlcohol[i]), dp[i - 2] + grapeAlcohol[i]);
+			}
+			Console.WriteLine(dp[n]);
+			
 			// Q11053 - 가장 긴 증가하는 부분 수열
-
-
+			int N = int.Parse(Console.ReadLine());
+			int[] sequence = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			Dictionary<int, int> dp = new Dictionary<int, int>();
+			for (int i = 0; i < N; i++)
+			{
+				bool gotThis = dp.ContainsKey(sequence[i]);
+				bool gotSmallerThings = dp.Keys.Any(s => s < sequence[i]);
+				if (gotThis == false)
+				{
+					if (gotSmallerThings == true)
+					{
+						dp[sequence[i]] = dp.Where(s => s.Key < sequence[i]).OrderBy(s=>s.Value).Last().Value + 1;
+					}
+					else
+					{
+						dp[sequence[i]] = 1;
+					}
+				}
+				else
+				{
+					if (gotSmallerThings == true)
+					{
+						dp[sequence[i]] = Math.Max(dp[sequence[i]], dp.Where(s => s.Key < sequence[i]).OrderBy(s => s.Value).Last().Value + 1);
+					}
+				}
+			}
+			Console.WriteLine(dp.Values.Max());
+			
 			// Q11054 - 가장 긴 바이토닉 부분 수열
-
-
+			int N = int.Parse(Console.ReadLine());
+			int[] inpArr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+			int[] fromZeroDp = new int[N];
+			int[] fromEndDp = new int[N];
+			for (int i = 0; i < N; i++)
+			{
+				int count = 0;
+				for (int j = 0; j < i; j++)
+				{
+					if (inpArr[i] > inpArr[j])
+					{
+						count = Math.Max(fromZeroDp[j], count);
+					}
+				}
+				fromZeroDp[i] = count + 1;
+			}
+			for (int i = N-1; i >=0; i--)
+			{
+				int count = 0;
+				for (int j = N-1; j > i; j--)
+				{
+					if (inpArr[i] > inpArr[j])
+					{
+						count = Math.Max(fromEndDp[j], count);
+					}
+				}
+				fromEndDp[i] = count + 1;
+			}
+			int maxBitonic = 0;
+			for (int i = 0; i < N; i++)
+			{
+				maxBitonic = Math.Max(fromEndDp[i] + fromZeroDp[i], maxBitonic);
+			}
+			Console.WriteLine(maxBitonic - 1);
+			*/
 			// Q2565 - 전깃줄
+			int N = int.Parse(Console.ReadLine());
+			Dictionary<string, List<string>> noNeedDp = new Dictionary<string, List<string>>();
+			for (int i = 0; i < N; i++)
+			{
+				string newElectronicLine = Console.ReadLine();
+				noNeedDp[newElectronicLine] = new List<string>();
+				int[] newLineIndex = Array.ConvertAll(newElectronicLine.Split(), int.Parse);
+				for (int j = 0; j < noNeedDp.Count; j++)
+				{
+					string jthLine = noNeedDp.ElementAt(j).Key;
+					int[] jthLineIndex = Array.ConvertAll(jthLine.Split(), int.Parse);
+					if ((newLineIndex[0] - jthLineIndex[0]) * (newLineIndex[1] - jthLineIndex[1]) < 0) // 엇갈렸으면
+					{
+						noNeedDp[newElectronicLine].Add(jthLine); // 새 전기줄에 겹치는 줄 추가
+						noNeedDp[jthLine].Add(newElectronicLine); // 겹치는 줄에 새 전기줄 추가
+					}
+				}
+			}
 
+			int removeCount = 0;
+			while (noNeedDp.Keys.Any(s => noNeedDp[s].Count > 0))
+			{
+				int maxCount = noNeedDp.Max(s => s.Value.Count);
+				string lineToRemove = noNeedDp.Where(s => s.Value.Count == maxCount).First().Key;
+				foreach (string lines in noNeedDp[lineToRemove])
+				{
+					noNeedDp[lines].Remove(lineToRemove); // 연결된놈에서 삭제할놈 삭제
+				}
+				noNeedDp[lineToRemove] = new List<string>(); // 삭제할놈이 가진 리스트 초기화
+				removeCount++;
+			}
+			Console.WriteLine(removeCount);
 
 			// Q9251 - LCS
 
