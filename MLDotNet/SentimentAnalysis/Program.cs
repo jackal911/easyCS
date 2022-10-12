da30ac8701f31d81b825c1d7a0544b211b7dedb0
Quantemp <77562615+jackal911@users.noreply.github.com>
2022-10-12 18:02:22 +0900

    something learn

---
+++ MLDotNet/SentimentAnalysis/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+MLContext mlContext = new MLContext();
+TrainTestData splitDataView = LoadData(mlContext);
+ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+
+TrainTestData LoadData(MLContext mlContext)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+    TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+    return splitDataView;
+}
+
+ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainSet)
+{
+    var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+    .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+}
